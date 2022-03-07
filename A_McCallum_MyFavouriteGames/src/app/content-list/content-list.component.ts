7e52ac7d4dfefecd65df2a7ec2e8c73288802db3
Ashley McCallum <w0765596@myscc.ca>
2022-03-07 14:53:35 -0500

    Begin assignment 6

--- A_McCallum_MyFavouriteGames/src/app/content-list/content-list.component.ts
+++ A_McCallum_MyFavouriteGames/src/app/content-list/content-list.component.ts
@@ -14,56 +14,7 @@ export class ContentListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.contentList = [{
-      id: 1,
-      title: "The Legend of Zelda: Breath of the Wild",
-      description: "Forget everything you know about The Legend of Zelda games. Step into a world of discovery, exploration, and adventure in The Legend of Zelda: Breath of the Wild, a boundary-breaking new game in the acclaimed series. Travel across vast fields, through forests, and to mountain peaks as you discover what has become of the kingdom of Hyrule in this stunning Open-Air Adventure.",
-      creator: "Nintendo",
-      imgURL: "https://assets.nintendo.com/image/upload/c_pad,f_auto,h_613,q_auto,w_1089/ncom/en_US/games/switch/t/the-legend-of-zelda-breath-of-the-wild-switch/hero?v=2022012808",
-      type: "Switch",
-      tags: ["Adventure", "Action", "Role-Playing"]
-    }, {
-      id: 2,
-      title: "Assassin's Creed Origins",
-      description: "Ancient Egypt, a land of majesty and intrigue, is disappearing in a ruthless fight for power. Unveil dark secrets and forgotten myths as you go back to the one founding moment: The Origins of the Assassin's Brotherhood.",
-      creator: "Ubisoft",
-      imgURL:"https://macgamesbox.com/wp-content/uploads/Assassins-Creed-Origins-Mac-OS-X.jpg",
-      tags: ["Open World", "Assassin", "Action", "RPG", "Stealth"]
-    }, {
-      id: 3,
-      title: "Fallout 4",
-      description: "As the sole survivor of Vault 111, you enter a world destroyed by nuclear war. Every second is a fight for survival, and every choice is yours. Only you can rebuild and determine the fate of the Wasteland. Welcome home.",
-      creator: "Bethesda",
-      type: "PC"
-    }, {
-      id: 4,
-      title: "Mass Effect Legendary Edition",
-      description: "One person is all that stands between humanity and the greatest threat it’s ever faced. Relive the legend of Commander Shepard in the highly acclaimed Mass Effect trilogy with the Mass Effect™ Legendary Edition.",
-      creator: "BioWare",
-      tags: ["RPG", "Story Rich", "Choices Matter", "Sci-fi", "Action"]
-    }, {
-      id: 5,
-      title: "Just Cause 3",
-      description: "The Mediterranean republic of Medici is suffering under the brutal control of General Di Ravello, a dictator with an insatiable appetite for power. Enter Rico Rodriguez, a man on a mission to destroy the General’s hold on power by any means necessary. With over 400 square miles of complete freedom from sky to seabed and a huge arsenal of weaponry, gadgets and vehicles, prepare to unleash chaos in the most creative and explosive ways you can imagine.",
-      creator: "Avalanche Studios",
-      imgURL: "https://cdn-ext.fanatical.com/production/product/1280x720/01de5e19-aa75-443a-8f96-0e49d26a1ace.jpeg",
-      type: "PC",
-      tags: ["Open World", "Action", "Destruction", "Multiplayer"]
-    }, {
-      id: 6,
-      title: "Animal Crossing: New Horizons",
-      description: "Escape to a deserted island and create your own paradise as you explore, create, and customize in the Animal Crossing: New Horizons game. Your island getaway has a wealth of natural resources that can be used to craft everything from tools to creature comforts. You can hunt down insects at the crack of dawn, decorate your paradise throughout the day, or enjoy sunset on the beach while fishing in the ocean. The time of day and season match real life, so each day on your island is a chance to check in and find new surprises all year round.",
-      creator: "Nintendo",
-      type: "Switch",
-      tags: ["Life Simulation", "Adventure", "Education"]
-    }, {
-      id: 7,
-      title: "Pokémon Legends: Arceus",
-      description: "Survey, catch, and research wild Pokémon in a long-gone era of Sinnoh to complete the region’s first Pokédex.",
-      creator: "Nintendo",
-      imgURL: "https://nintendoeverything.com/wp-content/uploads/Pokemon-Legends-Arceus-update-1.0.2.jpg",
-      type: "Switch"
-    }];
+
 
   }
 
